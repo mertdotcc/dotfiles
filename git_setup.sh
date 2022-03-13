aa39c5e7c490c4a6d058be31b5afbc13d7ae566f
Mert Ozturk <mert1ozturk@gmail.com>
2022-03-13 17:48:11 +0300

    add shell script for easy setup

---
+++ git_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+EMAIL_1="mert1ozturk"
+EMAIL_2="gmail.com"
+
+set -x
+
+# author
+git config --global user.name "Mert Ozturk"
+git config --global user.email "${EMAIL_1}@${EMAIL_2}"
+
+# ignore 
+git config --global core.excludesfile ~/.gitignore_global
+
+# editor
+# git config --global core.editor "vim"
+git config --global core.editor "code --wait"
+
+# branch
+git config --global init.defaultBranch "main"
