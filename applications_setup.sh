4c81a77196c2f659d0ac26f201e0285f7237f755
Mert Ozturk <mert1ozturk@gmail.com>
2022-03-13 18:56:04 +0300

    add general setup script

---
+++ applications_setup.sh
@@ -0,0 +1,40 @@
+# general
+# should return /opt/homebrew/bin/brew
+which brew
+brew analytics off
+
+# containers & kubernetes & tools
+brew install kubectl
+brew install kubectx
+brew install helm
+brew install docker
+brew install derailed/k9s/k9s
+
+# development & power tools
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install --cask goland
+brew install --cask intellij
+brew install --cask datagrip
+brew install go
+brew install node
+brew install python3
+brew install openssl
+brew install openssh
+brew install protobuf
+brew install prototool
+brew install watch
+brew install grep
+brew install cat
+
+# fun
+brew install asciinema
+
+# desktop software
+brew install zoomus
+brew install rectangle
+brew install topnotch
+brew install spotify
+brew install google-chrome
+brew install discord
+brew install obs
