f58ff2b37f08dd03550da709e13010ab28feed0a
Mert Öztürk <mertdotcc@gmail.com>
2023-03-29 15:59:35 +0200

    cleanup & updates

--- applications_setup.sh
+++ applications_setup.sh
@@ -1,41 +1,51 @@
-# general
-# should return /opt/homebrew/bin/brew
 which brew
 brew analytics off
 
-# containers & kubernetes & tools
-brew install kubectl
-brew install kubectx
-brew install helm
-brew install docker
-brew install derailed/k9s/k9s
-brew install jesseduffield/lazygit/lazygit
-
-# development & power tools
-brew install --cask iterm2
-brew install --cask visual-studio-code
+brew install --cask anki
+brew install --cask clocker
+brew install --cask docker
+brew install --cask finicky
 brew install --cask goland
-brew install --cask intellij
-brew install --cask datagrip
+brew install --cask google-chrome
+brew install --cask grammarly-desktop
+brew install --cask hammerspoon
+brew install --cask intellij-idea
+brew install --cask iterm2
+brew install --cask maccy
+brew install --cask microsoft-excel
+brew install --cask microsoft-outlook
+brew install --cask microsoft-powerpoint
+brew install --cask microsoft-word
+brew install --cask monitorcontrol
+brew install --cask nordvpn
+brew install --cask notion
+brew install --cask postman
+brew install --cask rectangle
+brew install --cask slack
+brew install --cask spotify
+brew install --cask stats
+brew install --cask visual-studio-code
+brew install --cask whatsapp
+brew install --cask zoom
+
+brew install aws-auth
+brew install awscli
+brew install azure-cli
+brew install difftastic
+brew install dive
+brew install gnu-sed
 brew install go
-brew install node
-brew install python3
-brew install openssl
-brew install openssh
-brew install protobuf
-brew install prototool
-brew install watch
 brew install grep
-brew install cat
-
-# fun
-brew install asciinema
-
-# desktop software
-brew install zoomus
-brew install rectangle
-brew install topnotch
-brew install spotify
-brew install google-chrome
-brew install discord
-brew install obs
+brew install helm
+brew install istioctl
+brew install iter8@0.13
+brew install k9s
+brew install k9s
+brew install kamel
+brew install kn
+brew install kubectx
+brew install kubernetes-cli
+brew install lazygit
+brew install quarkusio/tap/quarkus
+brew install watch
+brew tap iter8-tools/iter8
